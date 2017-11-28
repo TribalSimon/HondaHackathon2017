ef3073b8bd4108b69935a7680df39b9a3cc1c704
Simon Tsai <simon.tsai.j@gmail.com>
2017-11-28 09:49:06 -0800

    Added events.

--- HondaResponder/EventsViewController.swift
+++ HondaResponder/EventsViewController.swift
@@ -16,6 +16,8 @@ protocol EventsViewControllerDelegate {
 
 class EventsViewController: UIViewController {
     
+    private let titles = ["Basketball", "Cars", "Football"]
+    
     var delegate: EventsViewControllerDelegate?
     
 }
@@ -25,6 +27,36 @@ private extension EventsViewController {
     @IBAction private func closeTapped() {
         
         delegate?.closedTappedInEventsViewController(self)
+        
+    }
+    
+}
+
+extension EventsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell
+        
+        if let aCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            
+            cell = aCell
+            
+        } else {
+            
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            
+        }
+        
+        cell.textLabel?.text = titles[indexPath.row]
+        
+        return cell
         
     }
     
