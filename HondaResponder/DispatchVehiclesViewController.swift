b184f661cbeebc1f33dcdaaf89e60a4e85ba9569
Simon Tsai <simon.tsai.j@gmail.com>
2017-11-28 07:50:24 -0800

    Added a car in the dispatch bar on the bottom.

--- HondaResponder/DispatchVehiclesViewController.swift
+++ HondaResponder/DispatchVehiclesViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class DispatchVehiclesViewController: UIViewController {
     
+    @IBOutlet private weak var autoButton: UIButton!
     
+    private let car: UIView = {
+        
+        let car = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 35, height: 35)))
+        car.backgroundColor = .black
+        
+        return car
+        
+    }()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        view.addSubview(car)
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        super.viewWillLayoutSubviews()
+        
+        let originX = (autoButton.frame.minX + 24) / 2 - car.frame.width / 2
+        let originY = view.frame.height / 2 - car.frame.height / 2
+        
+        car.frame = CGRect(origin: CGPoint(x: originX, y: originY), size: car.frame.size)
+        
+    }
     
 }
