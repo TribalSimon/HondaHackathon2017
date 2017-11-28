d04986a347e5a52619dcb3c893d86c8b8639dda6
Simon Tsai <simon.tsai.j@gmail.com>
2017-11-27 19:02:49 -0800

    Added bottom bar as a child view controller.

---
+++ HondaResponder/DispatchVehiclesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DispatchVehiclesViewController.swift
+//  HondaResponder
+//
+//  Created by Simon Tsai on 11/27/17.
+//  Copyright © 2017 TribalScale. All rights reserved.
+//
+
+import UIKit
+
+class DispatchVehiclesViewController: UIViewController {
+    
+    
+    
+}
